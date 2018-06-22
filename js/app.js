ec4237b6cd2fc2fdf622c36a6f6e529b1bb530ae
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-21 21:06:25 -0600

    Commit 2 - I added more elements. My power grows with my dedication and hard work.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,28 @@ console.log("JavaScript is running...");
 	let $container = $("div").attr("id", "container");
 console.log($container);
 
+$($container).append("<h1/>");
+$("h1").text("Syllable skill-a-holic");
+
 $($container).append("<h2/>");
-$("h2").text("Syllable skill-a-holic");
+$("h2").text("Eric Todd");
+
+$($container).append("<h3/>");
+$("h3").text("Slytherin");
+
+$($container).append("<h4/>");
+$("h4").addClass("dog");
+$(".dog").text("Zephyr")
+
+// $($container).append("<h4/>");
+// $("h4").text("Holly Wood with Unicorn Hair Core");
+// ("Holly Wood with Unicorn Hair Core")
+
+// THIS SHIT DOESN'T WORK
+
+
+
+
 
 
 
