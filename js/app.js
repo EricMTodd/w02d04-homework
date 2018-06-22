da675521bc1f0a443b87300ee5486ca8bef18ab6
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-21 22:05:50 -0600

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos. Further correction of previous code.

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ $($container).append("<ul/>");
 $("ul").attr("storage", "trunk");
 $("ul").append("<li>Butter Beer</li><li class='secret'>Invisibility Cloak</li><li class='secret'>Marauder's Map</li><li class='secret'>Time Turner</li><li class='dog'>Leash</li><li>Bertie Bott's Every Flavor Beans</li>");
 
+$($container).append("<h5>Spring 2019</h5>")
+$($container).append("<table><thead><th>Day</th><th>Classes</th><tr><td>Monday</td><td>History of Magic, Defense Against the Dark Arts</td></tr><tr><td>Tuesday</td><td>Herbology, Potions</td></tr><tr><td>Wednesday</td><td>Transfiguration, Transmutation</td></tr><tr><td>Thursday</td><td>Divination, Charms</td></tr><tr><td>Friday</td><td>Dueling Club, Quidditch practice</td></tr></thead></table>");
 
 
 
