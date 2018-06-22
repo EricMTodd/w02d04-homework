b7c83fefb174c5182bd3621d5b5750086225593c
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-21 19:25:20 -0600

    Commit 1 - I added my first element and it was fun

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 console.log("JavaScript is running...");
 
+ $(() => {
+	let $container = $("div").attr("id", "container");
+console.log($container);
+
+$($container).append("<h2/>");
+$("h2").text("Syllable skill-a-holic");
+
+
+
+});
 
 
 
