5f7dcd2230fcd53ed1d7623121869ad590a9fcba
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-21 23:11:41 -0600

    Commit 5 - The days go by slowly and the years go by too fast.

--- js/app.js
+++ js/app.js
@@ -12,11 +12,15 @@ $($container).append("<h3>Slytherin</h3>");
 
 $($container).append("<h4 class='dog'>Zephyr</h4>");
 
-$($container).append("<h4>Holly Wood with Unicorn Hair Core</h4>");
+$($container).append("<h4 class='wand'>Holly Wood with Unicorn Hair Core</h4>");
+$("h4").remove(".wand");
+$($container).append("<h4 class='wand'>New Holly Wood with Unicorn Hair Core</h4>");
+$(".wand").css({"color": "indigo"});
 
 $($container).append("<ul/>");
 $("ul").attr("storage", "trunk");
-$("ul").append("<li>Butter Beer</li><li class='secret'>Invisibility Cloak</li><li class='secret'>Marauder's Map</li><li class='secret'>Time Turner</li><li class='dog'>Leash</li><li>Bertie Bott's Every Flavor Beans</li>");
+$("ul").append("<li class='beer'>Butter Beer</li><li class='secret'>Invisibility Cloak</li><li class='secret'>Marauder's Map</li><li class='secret'>Time Turner</li><li class='dog'>Leash</li><li>Bertie Bott's Every Flavor Beans</li>");
+$("li").remove(".beer")
 
 $($container).append("<h5>Spring 2019</h5>")
 $($container).append("<table><thead><th>Day</th><th>Classes</th><tr><td>Monday</td><td>History of Magic, Defense Against the Dark Arts</td></tr><tr><td>Tuesday</td><td>Herbology, Potions</td></tr><tr><td>Wednesday</td><td>Transfiguration, Transmutation</td></tr><tr><td>Thursday</td><td>Divination, Charms</td></tr><tr><td>Friday</td><td>Dueling Club, Quidditch practice</td></tr></thead></table>");
